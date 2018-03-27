f643043385d453937e42d21651eec648ff7bea7e
D.W <37800882+D-Wtnbe@users.noreply.github.com>
2018-03-27 17:28:14 +0900

    Add files via upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             activated: true,
-             activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+15.times do |n|
+  userid = "test#{n+1}"
+  name  = "テスト#{n+1}"
+  email = "example-#{n+1}@example.com"
   password = "password"
-  User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+  gender = 1
+  prefecture_code = "#{n+1}"
+  birthday = "1970-03-#{n+1}"
+  User.create!(userid: userid, name:  name,email: email, gender: gender, prefecture_code: prefecture_code,
+              password: password, birthday: birthday,
+              password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
+15.times do |n|
+  userid = "test#{n+1}"
+  name  = "テスト#{n+15}"
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  gender = 2
+  prefecture_code = "#{n+1}"
+  birthday = "1990-02-#{n+1}"
+  User.create!(userid: userid, name:  name,email: email, gender: gender, prefecture_code: prefecture_code,
+              password: password, birthday: birthday,
+              password_confirmation: password)
+end
+
+users = User.order(:created_at).take(30)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
